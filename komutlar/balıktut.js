657c2b835ad0c0f79d88a959b0c6fb3525544c61
necmiemir3 <40902291+necmiemir3@users.noreply.github.com>
2018-07-07 15:55:56 +0300

    Add files via upload

--- komutlar/balıktut.js
+++ komutlar/balıktut.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 exports.run = (client, message) => {
    message.channel.send('Balık Tuttun Balığı Çekiyorsun..').then(message => {
-   var espriler = ['Hamsi Tuttun!' ,'Levrek Tuttun!' ,'Hiçbirşey Tutamadın Maalesef!' ,'Alabalık Tuttun!' ,'Maalesef Balık Oltadan Kaçtı!' ,'İstavrit Tuttun!'];
+   var espriler = ['Sazan Tuttun!' ,'Köpek Balığı Tuttun İyi Para Eder Sat Sat :D' ,'Uskumru Tuttun!' ,'Mezgit Tuttun! Havyarıda Var hee ;)' ,'Japon Balığı Tuttun Yemeyi Düşünmüyorsun Herhalde?' ,'Hamsi Tuttun!' ,'Levrek Tuttun!' ,'Hiçbirşey Tutamadın Maalesef!' ,'Alabalık Tuttun!' ,'Maalesef Balık Oltadan Kaçtı!' ,'İstavrit Tuttun!'];
       var espri = espriler[Math.floor(Math.random() * espriler.length)];
             message.edit(`${espri}`);
  });
